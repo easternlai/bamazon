23feaa13e809d963ace104340679ecb579143b44
Eastern Lai <easternlai@hotmail.com>
2019-12-18 21:58:32 -0800

    working program

--- bamazon_DB.sql
+++ bamazon_DB.sql
@@ -7,7 +7,7 @@ CREATE TABLE products (
     item_id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(30) NOT NULL,
     department_name VARCHAR(30) NOT NULL,
-    price INT default 0,
+    price DECIMAL (10,2) default 0,
     stock_quantity INT default 0,
     PRIMARY KEY (item_id)
 );
--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -15,14 +15,11 @@ connection.connect(function(err) {
     startPrompt();
 });
 
-
-
-
-
 function startPrompt(){
 
     console.log("Displaying all products:... \n");
     var currentInventory = [];
+    var matchIndex;
     connection.query("SELECT * FROM products", function(err, res){
         
         for (i = 0; i < res.length; i++){
@@ -44,12 +41,31 @@ function startPrompt(){
                 message: "How many do you want to purchase?"
             }
         ]).then(function(inquireResponse){
-            console.log(inquireResponse.userItem);
+            for (i = 0; i < res.length; i++){
+                if(inquireResponse.userItem === res[i].product_name){
+                    matchIndex = res[i];
+                }
+            }
+            if (matchIndex.stock_quantity > inquireResponse.userQuantity){
+                connection.query("UPDATE products SET ? WHERE ?", 
+                [
+                    {
+                        stock_quantity: matchIndex.stock_quantity - inquireResponse.userQuantity
+                    },
+                    {
+                        item_id: matchIndex.item_id
+                    }
+                ], 
+                    function (error){
+                        if (error) throw err;
+                        console.log("Your purchase total is: $" + (matchIndex.price * inquireResponse.userQuantity).toFixed(2));
 
-
+                    }
+                );
+            }else{
+                console.log("Sorry your purchase request exceeds our current inventory of " + matchIndex.stock_quantity + ".");
+            }
         });
+
     });
 }
-
-// * The first should ask them the ID of the product they would like to buy.
-// * The second message should ask how many units of the product they would like to buy.
